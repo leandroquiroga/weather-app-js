e53866a6d2abf96f12b04d8becc6843d5b9ff299
Leandro <leandroquiroga9514@gmail.com>
2021-11-09 16:16:30 -0300

    Change fetch for Try..Catch

--- src/js/functions/listenners.js
+++ src/js/functions/listenners.js
@@ -32,11 +32,12 @@ export const eventListtener = () => {
 
 }
 
-function searchCity(city, form) {
+// Search the forescast extended by 5 days 
+async function searchCity(city, form) {
     let btnCity = selector('#btn-form');
     const apiKey = 'd62111581f103b598198001d275a170b';
     const url = (`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
-    fetch(url)
+/*     fetch(url)
         .then(response => response.json())
         .then(data => {
             if (data.cod === '404') {
@@ -49,4 +50,21 @@ function searchCity(city, form) {
             form.reset()
         
         })
+ */
+    
+    try {
+        const response = await fetch(url);
+        const result = await response.status
+        if (result === 404) {
+            messageErr('Ciudad Invalida', btnCity);
+            return
+        }
+        const data = await response.json();
+        addCountryArr(city)
+        dataWeatherCountry(data);
+        form.reset()
+
+    } catch (error) {
+        console.log(error)
+    }
 }
